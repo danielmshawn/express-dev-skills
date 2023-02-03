392cebb75c57a0e8b1e98aa23cbc9ec47a6de1b4
danielmshawn <danielmshawn@gmail.com>
2023-02-02 16:26:01 -0800

    add new and delete functionality

--- controllers/skills.js
+++ controllers/skills.js
@@ -3,9 +3,23 @@ const Skill = require('../models/skill');
 module.exports = {
     index,
     show,
-    new: newSkill
+    new: newSkill,
+    create,
+    delete: deleteSkill
 };
 
+function deleteSkill(req, res) {
+    Skill.deleteOne(req.params.id);
+    res.redirect('/skills');
+}
+
+function create(req, res) {
+    console.log(req.body);
+    //Models are responsible for CRUD'ing data
+    Skill.create(req.body);
+    // Always do a redirect when data has been changed
+    res.redirect('/skills');
+}
 
 function newSkill(req, res) {
     res.render('skills/new', {title: 'New Skill'});
--- models/skill.js
+++ models/skill.js
@@ -8,8 +8,23 @@ const skills = [
 	
   module.exports = {
     getAll,
-    getOne
+    getOne,
+    create,
+    deleteOne
   };
+
+function deleteOne(id) {
+id = parseInt(id);
+const idx = skills.findIndex(skill => skill.id === id);
+  skills.splice(idx, 1);
+}
+
+function create(skill) {
+  // Add the ID
+  skill.id = Date.now() % 1000000;
+  skill.learned = false;
+  skills.push(skill);
+}
 	
 function getOne(id) {
     id = parseInt(id);
